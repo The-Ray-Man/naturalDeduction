a8096eb5f2e4dc5ab56ec7ac71a27cdd5c983aa8
The-Ray-Man <ramonw@hotmail.ch>
2025-02-28 22:36:45 +0100

    add identifieres from rules

--- backend/src/lib/rule/mod.rs
+++ backend/src/lib/rule/mod.rs
@@ -1,7 +1,11 @@
 pub mod apply;
 pub mod rule_definition;
+use std::collections::BTreeSet;
+
 use serde::{Deserialize, Serialize};
 use utoipa::{IntoParams, ToSchema};
+
+use super::derivation::formula::Identifier;
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema, PartialEq, PartialOrd)]
 pub enum Rules {
@@ -78,4 +82,71 @@ pub struct DerivationRule {
     pub name: Rules,
     pub premises: Vec<RuleStatement>,
     pub conclusion: RuleStatement,
+}
+
+impl RuleFormula {
+    pub fn identifiers(&self) -> BTreeSet<RuleIdentifier> {
+        let mut ident = BTreeSet::new();
+        match self {
+            RuleFormula::Ident(rule_identifier) | RuleFormula::Not(rule_identifier) => {
+                ident.insert(rule_identifier.clone());
+            }
+            RuleFormula::And { lhs, rhs }
+            | RuleFormula::Or { lhs, rhs }
+            | RuleFormula::Imp { lhs, rhs } => {
+                ident.insert(lhs.clone());
+                ident.insert(rhs.clone());
+            }
+            RuleFormula::True | RuleFormula::False => {}
+            RuleFormula::Forall {
+                identifier,
+                formula,
+            } => {
+                ident.insert(identifier.clone());
+                ident.extend(formula.identifiers());
+            }
+            RuleFormula::Exists {
+                identifier,
+                formula,
+            } => {
+                ident.insert(identifier.clone());
+                ident.extend(formula.identifiers());
+            }
+            RuleFormula::Substitution {
+                identifier,
+                lhs,
+                rhs,
+            } => {
+                ident.insert(identifier.clone());
+                ident.insert(lhs.clone());
+                ident.insert(rhs.clone());
+            }
+        }
+        ident
+    }
+}
+
+impl RuleStatement {
+    pub fn identifiers(&self) -> BTreeSet<RuleIdentifier> {
+        let mut set = BTreeSet::new();
+        if let Some(lhs) = &self.lhs {
+            set.insert(lhs.clone());
+        }
+        set.extend(self.formula.identifiers());
+        set
+    }
+}
+
+impl DerivationRule {
+    pub fn identifiers(&self) -> BTreeSet<RuleIdentifier> {
+        let mut set = self
+            .premises
+            .iter()
+            .fold(BTreeSet::new(), |mut acc, premise| {
+                acc.extend(premise.identifiers());
+                acc
+            });
+        set.extend(self.conclusion.identifiers());
+        set
+    }
 }
